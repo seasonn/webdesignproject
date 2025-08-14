0c90761478217552fc11296b73e70faf0cf3d5aa
seasonn <tryinghelp513@gmail.com>
2025-08-14 10:41:27 +0800

    change dir of main.php

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+
+<head>
+    <?php require_once('components/head.php') ?>
+</head>
+
+<body>
+    <!-- back to top button -->
+    <?php require_once('components/backToTop.php') ?>
+
+    <!-- navbar -->
+    <?php require_once('components/navbar.php') ?>
+
+    <main>
+        <div class="container-fluid g-0">
+            <!-- breadcrumb -->
+            <?php require_once('components/breadcrumb.php') ?>
+            <!-- current products page -->
+            <?php require_once('components/products.php') ?>
+        </div>
+    </main>
+
+    <footer>
+        <?php require_once('components/footer.php') ?>
+    </footer>
+
+    <?php require_once('components/js.php') ?>
+</body>
+
+</html>
