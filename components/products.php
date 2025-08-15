02be31a05454883c127df3df196508e604091425
seasonn <tryinghelp513@gmail.com>
2025-08-15 13:28:49 +0800

    link db

--- components/products.php
+++ components/products.php
@@ -1,45 +1,61 @@
+<?php require_once('lib/php_lib.php'); ?>
 <div class="container mt-5 mb-4">
     <div hidden>source:https://digilog.tw/</div>
     <div class="product">
-        <h3 class="text-center my-3" id="">MIDI</h3>
+        <?php
+        //建立商品資料查詢
+        $maxRows_rs = 4; //分頁產品數
+        $pageNum_rs = 0; //起始頁
+        if (isset($_GET['pageNum_rs'])) {
+            $pageNum_rs = $_GET['pageNum_rs'];
+        }
+        $startRow_rs = $pageNum_rs * $maxRows_rs;
+
+        if (isset($_GET['classid'])) {
+            //使用產品類別查詢
+            $queryFirst = sprintf("SELECT * FROM product, product_img WHERE p_open=1 AND product_img.sort=1 AND product.p_id=product_img.p_id AND product.classid='%d' ORDER BY product.p_id DESC", $_GET['classid']);
+        } else {
+            //列出所有產品
+            $queryFirst = sprintf("SELECT * FROM product, product_img WHERE p_open=1 AND product_img.sort=1 AND product.p_id=product_img.p_id ORDER BY product.p_id DESC", $maxRows_rs);
+        }
+
+        $query = sprintf("%s LIMIT %d, %d", $queryFirst, $startRow_rs, $maxRows_rs);
+        $pList01 = $conn->query($query);
+        ?>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
-            
-            <div class="col">
-                <div class="card h-100 border-0">
-                    <img src="product_img/djtechtoolsmidifightertwister.png" class="card-img-top" alt="...">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <p class="card-title">DJ TechTools Midi Fighter Twister MIDI 控制器</p>
-                        <p class="card-text">8790 TWD</p>
+            <?php
+            while ($pList01_Rows = $pList01->fetch()) { ?>
+                <div class="col">
+                    <div class="card h-100 border-0">
+                        <img src="./product_img/<?= $pList01_Rows['img_file']; ?>" class="card-img-top" alt="<?= $pList01_Rows['p_name']; ?>" title="<?= $pList01_Rows['p_name']; ?>">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p class="card-title"><?= $pList01_Rows['p_name']; ?></p>
+                            <p class="card-text">NT<?= $pList01_Rows['p_price']; ?></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card h-100 border-0">
-                    <img src="product_img/NovationLaunchControlXLMKIIMIDI.png" class="card-img-top" alt="...">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <p class="card-title">Novation Launch Control XL MKII MIDI 控制器</p>
-                        <p class="card-text">6500 TWD</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100 border-0">
-                    <img src="product_img/PreSonusFaderPort8V2DAW.png" class="card-img-top" alt="...">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <p class="card-title">PreSonus FaderPort 8 V2 DAW 控制器</p>
-                        <p class="card-text">21500 TWD</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100 border-0">
-                    <img src="product_img/PreSonusFaderportV2DAW.png" class="card-img-top" alt="...">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                        <p class="card-title">PreSonus Faderport V2 DAW 控制器</p>
-                        <p class="card-text">7900 TWD</p>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
         </div>
+        <div class="row mt-2">
+            <?php
+            if (isset($_GET['totalRows_rs'])) {
+                $totalRows_rs = $GET['totalRows_rs'];
+            } else {
+                $all_rs = $conn->query($queryFirst);
+                $totalRows_rs = $all_rs->rowCount();
+            }
+            $totalPage_rs = ceil($totalRows_rs / $maxRows_rs) - 1;
+            $prev_rs = "&laquo;";
+            $next_rs = "&raquo;";
+            $seperator = "|";
+            $max_links = "20";
+            $pages_rs = buildNavigation($pageNum_rs, $totalPage_rs, $prev_rs, $next_rs, $seperator, $max_links, true, 3, "rs");
+            ?>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <?= $pages_rs[0] . $pages_rs[1] . $pages_rs[2]; ?>
+                </ul>
+            </nav>
+        </div>
+
     </div>
-</div>
